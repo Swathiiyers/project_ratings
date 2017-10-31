d13941f1078f43898012c7584398eb5d8cc98c82
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-31 15:34:37 -0700

    work in process on reg form

--- server.py
+++ server.py
@@ -2,10 +2,11 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, jsonify
+from flask import (Flask, jsonify, render_template, redirect, request, flash,
+                    session)
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -22,8 +23,34 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    a = jsonify([1,3])
-    return a
+    # a = jsonify([1,3])
+    # return a
+    return render_template("homepage.html")
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.route("/register", methods=["GET"])
+def user_registration():
+    """Send user to registration_form"""
+    return render_template("registration_form.html")
+
+
+@app.route("/register", methods=["POST"])
+def register_process():
+
+    email = request.form.get("frm_email")
+    password = request.form.get("frm_password")
+
+    check_email = int(User.query.filter_by(email=email).count())
+    print check_email
+
+    return check_email
+
 
 
 if __name__ == "__main__":
